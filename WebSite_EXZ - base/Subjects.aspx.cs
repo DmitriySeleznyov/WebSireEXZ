1ec1f426dd28d7c0cb4dc67fdb247728b2d88d41
Zubenko_Dmitriy <zubenko.dmitriy96@gmail.com>
2018-07-03 15:34:10 +0300

    Normal work with db. Updating Data.

--- WebSite_EXZ - base/Subjects.aspx.cs
+++ WebSite_EXZ - base/Subjects.aspx.cs
@@ -22,6 +22,7 @@ using Geocoding;
 using System.Reflection;
 using System.Runtime.Serialization.Formatters;
 using DevExpress.Utils.MVVM.Services;
+using System.Drawing;
 
 public partial class About : System.Web.UI.Page
 {
@@ -150,7 +151,6 @@ public partial class About : System.Web.UI.Page
             if (short_name != null)
             {
                 string sub_id = GetCompId();
-
             }
             else
             {
@@ -159,15 +159,11 @@ public partial class About : System.Web.UI.Page
         else
         {
         }
-
-
     }
 
     protected void Page_LoadComplete(object sender, EventArgs e)
     {
         RefreshRootFolders();
-
-
     }
 
     protected void Page_PreRender(object sender, EventArgs e)
@@ -177,13 +173,10 @@ public partial class About : System.Web.UI.Page
         if (short_name != null) //было Session["SelectedSubject"] вместо short_name
         {
             SubjectsBuild();
-
         }
         else
         {
         }
-
-
     }
 
     protected void Page_Init(object sender, EventArgs e)
@@ -198,7 +191,21 @@ public partial class About : System.Web.UI.Page
     }
 
     // ------------------ tab general
+    public byte[] jpgtobytea()
+    {
 
+        string path = Page.MapPath("~/") + String.Format("Images\\PicturesSubject\\temp.jpg"); ;
+        Bitmap image1 = (Bitmap)System.Drawing.Image.FromFile(path);
+        using (var ms = new MemoryStream())
+        {
+            image1.Save(ms, image1.RawFormat);
+            return ms.ToArray();
+        }
+    }
+    private string stringrefactor(string str)
+    {
+        return str.Replace(',', '.');
+    }
     protected void but_enter_Click(object sender, EventArgs e)
     {
         NpgsqlConnection myConn = new NpgsqlConnection(connString);
@@ -206,15 +213,25 @@ public partial class About : System.Web.UI.Page
         {
 
             short_name = GetShortName();
-
+            byte[] pic = jpgtobytea();
             string subject_insert = "insert into \"Subject\" (subject_name, subject_type, subject_code, object_name, add_info, loc, latitude, longitude) select '{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}'";
             string subject_update = "update \"Subject\" set subject_name='{0}', subject_type='{1}', subject_code='{2}', object_name='{3}', add_info='{4}', loc='{5}', latitude='{6}', longitude='{7}' where subject_id='{8}'";
 
-            var upsert_subject = new NpgsqlCommand(String.Format(String.Format("WITH upsert AS ({0} RETURNING *) {1} WHERE NOT EXISTS (SELECT * FROM upsert)", subject_update, subject_insert), tb_subject_name.Text, combox_subject_type.Text, tb_subject_code.Text, apostrof(Request.Form[tb_object_name.UniqueID]), apostrof(Request.Form[tb_subject_add_info.UniqueID]), apostrof(Request.Form[tb_subject_loc.UniqueID]), Request.Form[tb_subject_lat.UniqueID].Replace(",", "."), Request.Form[tb_subject_lon.UniqueID].Replace(",", "."), tb_subject_id.Text), myConn);
+            //var upsert_subject = new NpgsqlCommand(String.Format(String.Format("WITH upsert AS ({0} RETURNING *) {1} WHERE NOT EXISTS (SELECT * FROM upsert)", subject_update, subject_insert), tb_subject_name.Text, combox_subject_type.Text, tb_subject_code.Text, apostrof(Request.Form[tb_object_name.UniqueID]), apostrof(Request.Form[tb_subject_add_info.UniqueID]), apostrof(Request.Form[tb_subject_loc.UniqueID]), Request.Form[tb_subject_lat.UniqueID].Replace(",", "."), Request.Form[tb_subject_lon.UniqueID].Replace(",", "."), tb_subject_id.Text), myConn);
+            string queryinsert = "insert into \"Subject\" (subject_name, subject_type, subject_code, object_name, add_info, loc, latitude, longitude , picture) " +
+                "select '"+ tb_subject_name.Text+ "', '"+ combox_subject_type.Text+"', '"+tb_subject_code.Text+"', '"+ apostrof(Request.Form[tb_object_name.UniqueID])+"', '"
+                + apostrof(Request.Form[tb_subject_add_info.UniqueID]) + "', '"+ apostrof(Request.Form[tb_subject_loc.UniqueID])+"', '"
+                + stringrefactor(Request.Form[tb_subject_lat.UniqueID]) + "', '"+ stringrefactor(Request.Form[tb_subject_lon.UniqueID])+"' , '" +pic+ "'";
+            string queryupdate = "update \"Subject\" set " +
+                "subject_name='" + tb_subject_name.Text + "', subject_type='" + combox_subject_type.Text + "', subject_code='" + tb_subject_code.Text +
+                "', object_name='" + apostrof(Request.Form[tb_object_name.UniqueID]) + "', add_info='" + apostrof(Request.Form[tb_subject_add_info.UniqueID]) + "', loc='" 
+                + apostrof(Request.Form[tb_subject_loc.UniqueID]) + "', latitude='"  + stringrefactor(Request.Form[tb_subject_lat.UniqueID]) 
+                + "', longitude='" + stringrefactor(Request.Form[tb_subject_lon.UniqueID]) +"' , picture='"+ pic+ "' where subject_id='666'";
 
             myConn.Open();
-
-            upsert_subject.ExecuteNonQuery();
+            NpgsqlCommand np = new NpgsqlCommand(queryupdate, myConn);
+            np.ExecuteNonQuery();
+            //upsert_subject.ExecuteNonQuery();
 
 
         }
